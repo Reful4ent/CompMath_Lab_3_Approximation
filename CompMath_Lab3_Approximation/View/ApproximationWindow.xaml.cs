7e2b8e0debff365d0608b3415582e06bccd7ce50
Reful4ent <dima2323fr3@gmail.com>
2024-03-14 01:38:22 +0700

    Added a few VM classes, created GraphCalc in window and added TableOY in model dir

---
+++ CompMath_Lab3_Approximation/View/ApproximationWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CompMath_Lab3_Approximation.View
+{
+    /// <summary>
+    /// Логика взаимодействия для ApproximationWindow.xaml
+    /// </summary>
+    public partial class ApproximationWindow : Window
+    {
+        public ApproximationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ScotPlotStyle()
+        {
+            Graphics.Plot.Add.VerticalLine(0, 1, ScottPlot.Color.FromHex("#000000"));
+            Graphics.Plot.Add.HorizontalLine(0,1, ScottPlot.Color.FromHex("#000000"));
+            Graphics.Plot.XLabel("X");
+            Graphics.Plot.YLabel("Y");
+            Graphics.Refresh();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ScotPlotStyle();
+        }
+    }
+}
