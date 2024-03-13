d0a51ce7b817edf1b13efbe6d65ef960b10e7506
Reful4ent <dima2323fr3@gmail.com>
2024-03-13 23:53:45 +0700

    First commit

---
+++ CompMath_Lab3_Approximation/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CompMath_Lab3_Approximation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
